f0e9d6c247675b865beee37fa80646290eafdf9f
Mark Pinese <mark.pinese@gmail.com>
2020-09-15 13:31:34 +1000

    Switched to left-justified audio input

--- firmware/I2S to USB FS bridge/Core/Src/main.c
+++ firmware/I2S to USB FS bridge/Core/Src/main.c
@@ -202,7 +202,7 @@ static void MX_I2S2_Init(void)
   /* USER CODE END I2S2_Init 1 */
   hi2s2.Instance = SPI2;
   hi2s2.Init.Mode = I2S_MODE_SLAVE_RX;
-  hi2s2.Init.Standard = I2S_STANDARD_PHILIPS;
+  hi2s2.Init.Standard = I2S_STANDARD_MSB;
   hi2s2.Init.DataFormat = I2S_DATAFORMAT_24B;
   hi2s2.Init.MCLKOutput = I2S_MCLKOUTPUT_DISABLE;
   hi2s2.Init.AudioFreq = I2S_AUDIOFREQ_96K;
